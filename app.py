c003b23d6b2e98ec4a8099545f7c9bfbaf8455a2
kyoroxchan <kyoroxchan@gmail.com>
2024-02-19 19:28:28 +0900

    ユーザー周り少し

--- app.py
+++ app.py
@@ -1,16 +1,288 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, session, request, redirect, url_for
+import mysql.connector
+from datetime import timedelta
+from PIL import Image
+from datetime import datetime
+from werkzeug.utils import secure_filename
+import os
+import random
+import string
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+app.secret_key = "心がカギ"
+app.permanent_session_lifetime = timedelta(hours=24)
+app.config["MAX_CONTENT_LENGTH"] = 8**20
+
+
+# Gmailの設定
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
+app.config["MAIL_USERNAME"] = "nekocommu@gmail.com"
+app.config["MAIL_PASSWORD"] = "aeqy wxkm rkki cksr"
+app.config["MAIL_DEFAULT_SENDER"] = "nekocommu@gmail.com"
+
+mail = Mail(app)
+
+
+class DbOP:
+    def __init__(self, table):
+        self.__host = "localhost"
+        self.__user = "nekocommu"
+        self.__passwd = "neko"
+        self.__db = "nekocommu"
+        self.__table = table
+
+        self.__con = mysql.connector.connect(
+            host=self.__host,
+            user=self.__user,
+            passwd=self.__passwd,
+            db=self.__db,
+        )
+
+    def selectAll(self):
+        print("************************************************")
+
+        sql = "SELECT * FROM " + self.__table + ";"
+        print(sql)
+
+        cur = self.__con.cursor(dictionary=True)
+        cur.execute(sql)
+        res = cur.fetchall()
+        cur.close()
+        return res
+
+    # ログイン
+    def Login(self, mail):
+        print("************************************************")
+
+        sql = 'SELECT id,pass,mail FROM user WHERE mail ="' + mail + '";'
+        print(sql)
+
+        cur = self.__con.cursor(dictionary=True)
+        cur.execute(sql)
+        res = cur.fetchall()
+        cur.close()
+        return res
+
+    # ユーザーデータ抽出
+    def user(self, id):
+        print("************************************************")
+
+        sql = 'SELECT * FROM user WHERE id ="' + id + '";'
+        print(sql)
+
+        cur = self.__con.cursor(dictionary=True)
+        cur.execute(sql)
+        res = cur.fetchall()
+        cur.close()
+        return res
+
+    # 新規登録ID重複チェック
+    def idCheck(self):
+        print("************************************************")
+
+        sql = "SELECT id FROM user ;"
+        print(sql)
+
+        cur = self.__con.cursor(dictionary=True)
+        cur.execute(sql)
+        res = cur.fetchall()
+        cur.close()
+        return res
+
+    # DB接続＆絞込条件抽出
+    def selectEx(self, ex):
+
+        sql = "SELECT * FROM " + self.__table + " WHERE" + ex + ";"
+        print(sql)
+        cur = self.__con.cursor(dictionary=True)
+        cur.execute(sql)
+        res = cur.fetchall()
+        cur.close()
+
+        # ２次元配列の０行目（１件）のみを返す
+        return res[0]
+
+    # DB接続＆データ挿入
+    def insTbl(self, val):
+        sql = "INSERT INTO " + self.__table + " VALUES("
+        sql += val
+        sql += ",'');"
+        print(sql)
+        cur = self.__con.cursor()
+        cur.execute(sql)
+        self.__con.commit()
+        cur.close()
+
+    # DB接続＆データ削除
+    def delTbl(self, re):
+        sql = "DELETE FROM " + self.__table + " WHERE"
+        sql += re
+        sql += ";"
+        print(sql)
+        cur = self.__con.cursor()
+        cur.execute(sql)
+        self.__con.commit()
+        cur.close()
+
+    # DB切断
+    def close(self):
+        self.__con.close()
 
 
 @app.route("/")
 def home():
+    session.pop("mail", None)
+    try:
+        dbop = DbOP("post")
+        result = dbop.selectAll()
+        dbop.close()
+
+        return render_template("home.html", result=result)
+    except mysql.connector.errors.ProgrammingError as e:
+        print("***DB接続エラー***")
+        print(type(e))
+        print(e)
+    except Exception as e:
+        print("***システム運行プログラムエラー***")
+        print(type(e))
+        print(e)
+
     return render_template("home.html")
+
 
 @app.route("/user")
 def user():
-    return render_template("myprofile.html")
+    if "userId" in session:
+        try:
+            id = session["userId"]
+            print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+            dbop = DbOP("user")
+            result = dbop.user(id)
+            dbop.close()
+
+            return render_template("myprofile.html", result=result)
+
+        except mysql.connector.errors.ProgrammingError as e:
+            print("***** DB接続エラー *****")
+            print(type(e))
+            print(e)
+        except Exception as e:
+            print("***** システム運行プログラムエラー *****")
+            print(type(e))
+            print(e)
+    else:
+        test = {}
+        return render_template("login.html", test=test)
+
+
+@app.route("/post")
+def post():
+    return render_template("post.html")
+
+
+@app.route("/loginCheck", methods=["POST"])
+def loginCheck():
+    try:
+        result = {}
+        test = request.form
+        mail = test["mail"]
+        err = ""
+        print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+        dbop = DbOP("user")
+        result = dbop.Login(mail)
+        dbop.close()
+
+        if not result:
+            err = "メールアドレスまたはパスワードが違います"
+            return render_template("login.html", err=err, test=test)
+        for rec in result:
+            print(rec["pass"])
+            id = rec["id"]
+        if not test["pass"] == rec["pass"]:
+            err = "メールアドレスまたはパスワードが違います"
+            return render_template("login.html", result=result, err=err, test=test)
+
+        session["userId"] = id
+        return render_template("home.html", id=id)
+
+    except mysql.connector.errors.ProgrammingError as e:
+        print("***** DB接続エラー *****")
+        print(type(e))
+        print(e)
+    except Exception as e:
+        print("***** システム運行プログラムエラー *****")
+        print(type(e))
+        print(e)
+
+
+@app.route("/signup")
+def signup():
+
+    return render_template("signup.html")
+
+
+@app.route("/signupMail", methods=["POST"])
+def signupMail():
+    result = request.form
+    to_email = result["mail"]
+    subject = "ねここみゅアカウント新規登録確認メール"
+
+    # 画像を "static" フォルダ内に保存しておく
+    # img_path = "./static/furniture_mail.jpg"
+
+    body = render_template("email_template.html", to_email=to_email)
+
+    message = Message(subject=subject, recipients=[to_email], html=body)
+
+    mail.send(message)
+    return render_template("signupMail.html", to_email=to_email)
+
+
+@app.route("/signupSetting/<mail>")
+def signupSetting(mail):
+    session["mail"] = mail
+    return render_template("signupSetting.html")
+
+
+@app.route("/signupCheck", methods=["POST"])
+def signupCheck():
+    mail = session["mail"]
+    result = request.form
+    err = ""
+
+    if not result["pass"] == result["pass2"]:
+
+        return render_template("signupSetting.html", result=result)
+
+    try:
+        print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+        dbop = DbOP("user")
+        idCheck = dbop.idCheck()
+        dbop.close()
+        if result["id"] in idCheck:
+            return render_template("signupSetting.html", result=result, err=err)
+        session["name"] = result["name"]
+        session["id"] = result["id"]
+        return render_template("signupCheck.html", result=result, mail=mail)
+
+    except mysql.connector.errors.ProgrammingError as e:
+        print("***** DB接続エラー *****")
+        print(type(e))
+        print(e)
+    except Exception as e:
+        print("***** システム運行プログラムエラー *****")
+        print(type(e))
+        print(e)
+
+
+@app.route("/signout")
+def signout():
+    session.clear()
+    return render_template("signout.html")
 
 
 if __name__ == "__main__":
