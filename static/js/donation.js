34c483b8e765768658332caa9b795b418f25a2a5
kyoroxchan <kyoroxchan@gmail.com>
2024-02-28 12:38:00 +0900

    寄付機能一ページ目完成

--- static/js/donation.js
+++ static/js/donation.js
@@ -7,4 +7,18 @@ function valueChange(event) {
     let checkValue = selectForm.elements['addPoint'].value;
     console.log(checkValue);
     resultPoint.innerHTML = checkValue;
+    document.getElementById( "hiddenText" ).value = checkValue ;
 }
+
+
+
+//モーダル表示
+$(".modal-open").modaal({
+    overlay_close: true,//モーダル背景クリック時に閉じるか
+    before_open: function () {// モーダルが開く前に行う動作
+        $('html').css('overflow-y', 'hidden');/*縦スクロールバーを出さない*/
+    },
+    after_close: function () {// モーダルが閉じた後に行う動作
+        $('html').css('overflow-y', 'scroll');/*縦スクロールバーを出す*/
+    }
+});
